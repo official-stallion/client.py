6987a3ddece16a4cfc6f4c95d4d30b7107af898a
amirhnajafiz <najafizadeh21@gmail.com>
2022-08-16 11:59:11 +0430

    init: sdk

---
+++ __init__.py
@@ -0,0 +1,10 @@
+"""
+SDK for Stallion message broker clients.
+
+Check Stallion message broker: https://github.com/amirhnajafiz/stallion
+
+@version v1.0.0
+@author amirhnajafiz
+@email najafizadeh21@gmail.com
+@year 2022
+"""
