5a5e2e85b1a9f502afa569bbd1c8fec33c4d585f
amirhnajafiz <najafizadeh21@gmail.com>
2022-08-17 19:33:41 +0430

    feat: client methods

--- client.py
+++ client.py
@@ -7,3 +7,12 @@ from network import Network
 class Client:
     def __init__(self):
         self.net = Network("127.0.0.1", "7025")
+    
+    def Publish(self, topic, data):
+        pass
+
+    def Subscribe(self, topic, handler):
+        pass
+
+    def Unsubscribe(self, topic):
+        pass
