c0eb1bdb7c302f3fa3be62a570e91033540b2751
amirhnajafiz <najafizadeh21@gmail.com>
2022-08-19 17:22:43 +0430

    bug: in parsing

--- test.py
+++ test.py
@@ -10,9 +10,5 @@ def handler(data):
 if __name__ == "__main__":
     c = Client(url="localhost:9090")
     c.Subscribe("book", handler)
-
-    sleep(2)
-
     c.Publish("book", "new book")
-    sleep(2)
-    
+    c.Publish("book", "new book")
