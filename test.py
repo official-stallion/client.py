b108c70de167237740d00e23990f5802b183aa37
amirhnajafiz <najafizadeh21@gmail.com>
2022-08-19 18:23:50 +0430

    add: tests

--- test.py
+++ test.py
@@ -3,14 +3,23 @@ from client import Client
 
 
 
+"""
+creating a handler.
+"""
 def handler(data):
     print(data)
 
 
 if __name__ == "__main__":
+    # create client
     c = Client(url="localhost:9090")
+
     sleep(1)
+
+    # subscribe over a topic
     c.Subscribe("book", handler)
+
     sleep(1)
-    c.Publish("book", {'a': 'a', 'b': 'b'})
-    # c.Publish("book", "new book")
+
+    # publish messages over a topic
+    c.Publish("book", {'author': "Amirhossein", 'name': "Stallion"})
