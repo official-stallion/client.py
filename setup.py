96106adb415be6956632d65cd270089cc0c0a071
amirhnajafiz <najafizadeh21@gmail.com>
2022-08-20 08:51:00 +0430

    feat: pip files

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='example_publish_pypi_medium',
+    version='0.6',
+    license='MIT',
+    author="Giorgos Myrianthous",
+    author_email='email@example.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/gmyrianthous/example-publish-pypi',
+    keywords='example project',
+    install_requires=[
+          'scikit-learn',
+      ],
+
+)
