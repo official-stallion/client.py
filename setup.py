f42a40b30dc64ef795561a3121176988ca6dec76
amirhnajafiz <najafizadeh21@gmail.com>
2022-08-20 12:09:43 +0430

    update: setup

--- setup.py
+++ setup.py
@@ -3,12 +3,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='stallion-python-sdk',
-    version='1.1',
+    version='1.2',
     license='MIT',
     author="amirhnajafiz",
     author_email='najafizadeh21@gmail.com',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
+    packages=['stallion-python-sdk'],
     url='https://github.com/amirhnajafiz/stallion-python-sdk',
-    keywords='stallion sdk python',
+    keywords=['sdk', 'stallion', 'stallion-client'],
 )
